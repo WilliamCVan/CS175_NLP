6047ebaf472813fd0bc6790138ac60bbf51efa6b
Brandon Ha <bha3@uci.edu>
2021-02-07 19:34:24 -0800

    changed from list to dict for genfilewikipedia, added tokenizer for parsepairfiles (spacy)

--- genFileWikipedia.py
+++ genFileWikipedia.py
@@ -5,7 +5,7 @@ import os
 # https://docs.python.org/3/library/xml.etree.elementtree.html
 
 def wikiCreateJapEnglishPairList() -> list:
-    listPairs = list()
+    listPairs = dict()
 
     for path, subdirs, files in os.walk("./datafiles/wikipedia"):
         # if(path not in {"./datafiles/wikipedia\\CLT"}): # only include "culture" category
@@ -22,8 +22,8 @@ def wikiCreateJapEnglishPairList() -> list:
                 corpus = ET.parse(os.path.join(path, name)).getroot()
 
                 #from the root, get all children that follow this node heirarchy, two different types of text blocks so two different for loops
-                print(len(corpus.findall('par/sen')))
-                print(len(corpus.findall('sec/par/sen')))
+                # print(len(corpus.findall('par/sen')))
+                # print(len(corpus.findall('sec/par/sen')))
 
                 for type_tag in corpus.findall('par/sen'):
                     value = list(type_tag) # get all children elements under the <sen> tag
@@ -31,7 +31,7 @@ def wikiCreateJapEnglishPairList() -> list:
                     english_child = value[5].text # 6th child
 
                     # build [japanese, english] pairs
-                    listPairs.append([english_child, japanese_child])
+                    listPairs[english_child] = japanese_child
 
 
                 for type_tag in corpus.findall('sec/par/sen'):
@@ -40,12 +40,12 @@ def wikiCreateJapEnglishPairList() -> list:
                     english_child = value[5].text # 6th child
 
                     # build [japanese, english] pairs
-                    listPairs.append([english_child, japanese_child])
+                    listPairs[english_child] = japanese_child
 
 
             # write pairs out to file using <tab> as delimiter to match "standford_raw" dataset
             with open("./datafiles/wikipedia_raw", mode="a", encoding="utf-8") as file_in:
-                for eng, jap in listPairs:
+                for eng, jap in listPairs.items():
                     file_in.write(eng + "\t" + jap + "\n")
         except:
             pass
--- parsePairFiles.py
+++ parsePairFiles.py
@@ -1,22 +1,22 @@
-def processJapEnglishPairList() -> list:
-    listPairs = list()
+import spacy
 
-    with open("./datafiles/standford_raw", mode="r", encoding="utf-8") as file_in:
-        listPairs = list()
+nlp = spacy.load("ja_core_news_sm")
+
+def processJapEnglishPairList(filename) -> list:
+    tokens = list()
+
+    with open(filename, mode="r", encoding="utf-8") as file_in:
+        listPairs = dict()
         for line in file_in:
             eng, jap = line.split("\t")
-            listPairs.append([jap, eng])
-    # add tokenization/preprocessing logic
+            print("input: ", jap)
+            sen = nlp(jap)
+            for token in sen:
+                print(token.text, token.pos_, token.dep_)
+                tokens.append(token)
 
-
-    with open("./datafiles/wikipedia_raw", mode="r", encoding="utf-8") as file_in:
-        listPairs = list()
-        for line in file_in:
-            eng, jap = line.split("\t")
-            listPairs.append([jap, eng])
-
-    # add tokenization/preprocessing logic
 
 
 if __name__ == "__main__":
-    processJapEnglishPairList()
+    processJapEnglishPairList("./datafiles/wikipedia_raw")
+    # processJapEnglishPairList("./datafiles/stanford_raw")
